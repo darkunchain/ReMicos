7c6a6bc85c5a54e9d71768157e33bbf6175f55aa
Darkunchain <luisfer74081@yahoo.es>
2021-07-28 23:39:17 -0500

    listo por hoy

--- Backend/src/models/user.js
+++ Backend/src/models/user.js
@@ -5,6 +5,21 @@ const userSchema = new Schema({
     password: String
 },{
     timestamps: true
-})
+},{
+    roles: [{
+        ref: "Roles",
+        type: Schema.Types.ObjectId
+    }]
+});
+
+userSchema.statics.encryptPassword = async (password) => {
+    const salt = await bcrypt.genSalt(10)
+    return await bcrypt.hash(password, salt)
+}
+
+userSchema.statics.comparePassword = async (password, passRecibido) => {
+    return await bcrypt.compare(password, passRecibido)
+
+}
 
 module.exports = model('User',userSchema)
