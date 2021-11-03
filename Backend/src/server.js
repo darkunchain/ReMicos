3fb29b99501ab7583963e85d0322d819d9e520b6
darkunchain <luisfer74081@gmail.com>
2021-11-03 16:57:19 -0500

    ss

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const path = require('path')
+const cors = require('cors');
 require('dotenv').config();
 require('./db')
+
 
 crearRoles = require('./funciones/crearRoles');
 
@@ -18,9 +20,12 @@ app.use('/api',require('./routes/asientos'))
 app.use('/api',require('./routes/registros'))
 app.use('/api',require('./routes/users'))
 
+app.use(cors());
+app.options('*', cors());
+
 
 //configuraciones
-app.set('port',process.env.PORT || 4200)
+app.set('port',process.env.PORT || 3000)
 app.listen(app.get('port'), ()=>{
     console.log('Server on port ',app.get('port'))
 })
