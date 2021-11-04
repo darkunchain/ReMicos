a8fed7b96b8963e3a7b95ae9de22b5d615b43a1e
darkunchain <luisfer74081@gmail.com>
2021-11-04 15:17:23 -0500

    cors

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -23,6 +23,21 @@ app.use('/api',require('./routes/users'))
 app.use(cors());
 app.options('*', cors());
 
+var allowCrossDomain = function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Cache-Control");
+
+    // intercept OPTIONS method
+    if ('OPTIONS' == req.method) {
+      res.send(200);
+    }
+    else {
+      next();
+    }
+};
+app.use(allowCrossDomain);
+
 
 //configuraciones
 app.set('port',process.env.PORT || 8081)
