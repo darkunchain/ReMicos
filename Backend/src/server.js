d774e370e85009185eba657f95ca858e6a5655ba
darkunchain <luisfer74081@gmail.com>
2021-11-06 21:52:11 -0500

    gf

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -26,10 +26,10 @@ app.options('*', cors());
 
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");  
+  res.header("Access-Control-Allow-Origin", "http://localhost:4200");  
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, authorization"
+    "X-Requested-With, Content-Type, Accept"
   );
   res.header(
     "Access-Control-Allow-Methods",
