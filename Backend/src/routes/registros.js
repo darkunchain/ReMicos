927db2931954b938d96baa4d6e0f600720324e9e
darkunchain <luisfer74081@gmail.com>
2021-11-09 17:30:53 -0500

    datos

--- Backend/src/routes/registros.js
+++ Backend/src/routes/registros.js
@@ -4,44 +4,43 @@ const router = Router()
 const Registro = require('../models/registros')
 
 
-router.post('/add', async (req,res) => {
-    
+router.post('/add', async (req, res) => {
+
     console.log(req.body)
     const newRegistro = new Registro(req.body);
     await newRegistro.save();
-   
+
     res.redirect('/')
-    
+
 })
 
 router.get('/registros', async (req, res) => {
     //const Clientes = await Registro.findById(req.params.userid)
     const Clientes = await Registro.find()
-    
-    
-    const aggre = await Registro.aggregate([
-        { "$dateFromString": {
-            "dateString": "Date"            
-       } },
-        
+
+
+    const aggre = await Registro.aggregate([       
+
         {
-           "$project": {
-              "DueDateWeek": { "$week": "$Date" },
-              "DueDateMonth": { "$month": "$Date" },
-              "Rank": 1
-           }
+            "$project": {
+                yearMonthDayUTC: { $dateToString: { format: "%Y-%m-%d", date: "$date" } },
+                "DueDateWeek": { "$week": "$Date" },
+                "DueDateMonth": { "$month": "$Date" },
+                "Rank": 1
+            }
         },
-     {
-        "$group": {
-           "_id": "$DueDateWeek",
-           "AvgValue": { "$avg": "$Rank" },
-           "MonthValue": { "$first": "$DueDateMonth" }
-           }
+        {
+            "$group": {
+                "_id": "$DueDateWeek",
+                "AvgValue": { "$avg": "$Rank" },
+                "MonthValue": { "$first": "$DueDateMonth" },
+                "Fecha":yearMonthDayUTC
+            }
         }
-     ])
+    ])
 
 
-    res.status(200).send({Clientes, aggre})
+    res.status(200).send({ Clientes, aggre })
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
