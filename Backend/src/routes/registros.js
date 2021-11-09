5e8874d6d7fa14cd0c57e72482f409bb7a44047a
darkunchain <luisfer74081@gmail.com>
2021-11-09 16:58:48 -0500

    cam

--- Backend/src/routes/registros.js
+++ Backend/src/routes/registros.js
@@ -16,8 +16,26 @@ router.post('/add', async (req,res) => {
 
 router.get('/registros', async (req, res) => {
     //const Clientes = await Registro.findById(req.params.userid)
-    const Clientes = await Registro.find()    
-    res.status(200).send(Clientes)
+    const Clientes = await Registro.find()
+    const aggre = await Registro.aggregate([
+        {
+           "$project": {
+              "DueDateWeek": { "$week": "$Date" },
+              "DueDateMonth": { "$month": "$Date" },
+              "Rank": 1
+           }
+        },
+     {
+        "$group": {
+           "_id": "$DueDateWeek",
+           "AvgValue": { "$avg": "$Rank" },
+           "MonthValue": { "$first": "$DueDateMonth" }
+           }
+        }
+     ])
+
+
+    res.status(200).send({Clientes, aggre})
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "X-Requested-With");
     next();
