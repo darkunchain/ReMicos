44b354acf7696eaba1d7f33a94db6c0aa7601903
darkunchain <luisfer74081@gmail.com>
2022-01-27 11:54:04 -0500

    ajuste db

--- Backend/src/db.js
+++ Backend/src/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://app.remicos.com.co/remicos,localhost/remicos',{
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://app.remicos.com.co/remicos',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
