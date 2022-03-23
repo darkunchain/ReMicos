fbea357db8c73b29b20fc4051ac29dbd2345c119
darkunchain <luisfer74081@gmail.com>
2022-03-22 23:13:58 -0500

    error redime

--- Backend/src/routes/promos.js
+++ Backend/src/routes/promos.js
@@ -63,35 +63,35 @@ router.post('/redime', async (req, res) => {
     console.log('codAprob', codAprob, 'id', id)
 
 
-    
-    const codRemicosAll = await Aprob.findOne({"establecimiento": "Remicos"})
-    const codCaprichosAll = await Aprob.findOne({"establecimiento": "Caprichos"})    
 
-    
+    const codRemicosAll = await Aprob.findOne({ "establecimiento": "Remicos" })
+    const codCaprichosAll = await Aprob.findOne({ "establecimiento": "Caprichos" })
+
+
     const codRemicosDec = await Aprob.comparePassword(codAprob, codRemicosAll.codigo)
     const codCaprichosDec = await Aprob.comparePassword(codAprob, codCaprichosAll.codigo)
 
-    console.log(' codRemicosDec:',codRemicosDec, ' codCaprichosDec:',codCaprichosDec)
-    
-    if(!codRemicosDec) {
+    console.log(' codRemicosDec:', codRemicosDec, ' codCaprichosDec:', codCaprichosDec)
+
+    if (!codRemicosDec) {
         console.log('remicos falso')
-        if(!codCaprichosDec) {
-            console.log('caprichos falso')           
+        if (!codCaprichosDec) {
+            console.log('caprichos falso')
             msg = { errorMsg: "Este codigo de aprobación no es valido" }
             rend = "error_codigo"
             //return res.status(401).send('Este codigo de aprobación no es valido')
-            return res.render(rend, {msg, id});
-        }else {
+            return res.render(rend, { msg, id });
+        } else {
             establec = "Caprichos"
             redim = "redCaprichos"
         }
 
-        console.log(' establec:',establec)
-        
-    }else {
+        console.log(' establec:', establec)
+
+    } else {
         establec = "Remicos"
         redim = "redRemicos"
-        
+
 
         const promo = await promos.findById(id, function (err, doc) {
             if (err) {
@@ -102,62 +102,64 @@ router.post('/redime', async (req, res) => {
                     msg = { errorMsg: "Este bono no existe o ya fue redimido" }
                     rend = 'error'
                 } else {
-                    if(promo.redCaprichos && promo.redRemicos){
-                        msg = { errorMsg: "Este bono ya fue redimido en ambos establecimientos" }
-                        rend = 'error'
-                    }else{
-                        valid = true
-                        rend = 'redime'
-                    }
-                                    
+                    valid = true
+                    rend = 'redime'
                 }
             }
         })
 
+
+
         if (valid) {
-            msg = { id }
-            rend = 'redime'
-            const actualiza = await promos.findByIdAndUpdate(id, {redim: true}, function(err, result){
-                if (err) {
-                    msg = { errorMsg: "Ha ocurrido un error Inesperado" }
-                    rend = 'error'
-                } else {
-                    console.log('redime actualizado:', result)
-                }
-            })
-            
+            if (promo.redCaprichos && promo.redRemicos) {
+                msg = { errorMsg: "Este bono ya fue redimido en ambos establecimientos" }
+                rend = 'error'
+            } else {
+                msg = { id }
+                rend = 'redime'
+                const actualiza = await promos.findByIdAndUpdate(id, { redim: true }, function (err, result) {
+                    if (err) {
+                        msg = { errorMsg: "Ha ocurrido un error Inesperado" }
+                        rend = 'error'
+                    } else {
+                        console.log('redime actualizado:', result)
+                    }
+                })
+            }
+
+
         }
-    
-        
-        
-        
+
+
+
+
     }
-    
-    console.log(' establecAfuera:',establec)
 
-    
+    console.log(' establecAfuera:', establec)
 
-    res.status(200).render('okis',{establec})
+
+
+    res.status(200).render('okis', { establec })
 
 })
 
- 
+
 //%%%%%%%%%%%%%%%%%%%%%% Codigos de aprobacion %%%%%%%%%%%%%%%%%%%%%%%%//
-router.post('/promos/signup', async (req, res) => {    
-    const {codigo,establecimiento} = req.body;    
-    
-    const codeValidate = await Aprob.findOne({codigo})    
-    if(codeValidate) return res.status(401).send("este codigo ya existe, por favor seleccione otro");
-    const newCodigo = new Aprob({ codigo,establecimiento});
-       
-    newCodigo.codigo = await Aprob.encryptPassword(codigo)     
-    
-    await newCodigo.save();    
-    const token = jwt.sign({_id:newCodigo._id}, process.env.SECRET_ENC);
-    res.status(201).json({token})
+router.post('/promos/signup', async (req, res) => {
+    const { codigo, establecimiento } = req.body;
+
+    const codeValidate = await Aprob.findOne({ codigo })
+    if (codeValidate) return res.status(401).send("este codigo ya existe, por favor seleccione otro");
+    const newCodigo = new Aprob({ codigo, establecimiento });
+
+    newCodigo.codigo = await Aprob.encryptPassword(codigo)
+
+    await newCodigo.save();
+    const token = jwt.sign({ _id: newCodigo._id }, process.env.SECRET_ENC);
+    res.status(201).json({ token })
 })
 
- 
+
 
 
 
