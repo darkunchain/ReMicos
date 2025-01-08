505fa84273759c823891cf3fd291521306e01f53
lfprieto <lfprieto@registraduria.gov.co>
2025-01-08 14:17:24 -0500

    borrar fronend fake

--- Frontend/remicos/src/app/models/cliente.ts
+++
@@ -1,6 +0,0 @@
-export class cliente{
-  id:number=0;
-  nombre:string='';
-  telefono:number=0;
-  card:number=0
-}
--- Frontend/remicos/src/app/models/usuario.ts
+++
@@ -1,6 +0,0 @@
-export class usuario{
-  id:number=0;
-  nombre:string='';
-  correo:string='';
-  rol:string=''
-}
