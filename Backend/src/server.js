6b7494ef60925efc1071e92140dd525d74ce9ca4
Don Fernando <luisfer74081@gmail.com>
2026-09-16 17:04:05 -0500

    localhost port 3000

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -61,7 +61,7 @@ app.set('port',process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'));
 app.set('public', path.join(__dirname, 'public'));
 app.set('view engine', 'ejs');
-app.listen(app.get('port'), ()=>{
+app.listen(app.get('port'), '127.0.0.1', ()=>{
     console.log('Server on port ',app.get('port'))
 })
 
